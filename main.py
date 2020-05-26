d0f8aaaae3164d025d3cedd891477606c58711e5
kulychok <58169325+kulychok@users.noreply.github.com>
2020-05-26 11:55:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,31 +10,26 @@ import os
 from flask import Flask, request
 import logging
 
+# telegram API token
 bot = telebot.TeleBot('1099895310:AAHaLPowlJMPqGwsDwG27D45lvdqo8goNkw')
-# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
-
+# server
 server = Flask(__name__)
 
-
+# mongodb token
 random_bot_client = pymongo.MongoClient(
     'mongodb+srv://kulyk:c87hZXqIJEyz5LL5@cluster0-gqsxu.mongodb.net/test?retryWrites=true&w=majority'
 )
-
 database = random_bot_client.test
 collection = database.users
 
-
-
-print("MongoDB version is %s" %
-            random_bot_client.server_info()['version'])
-
-numbers = [0, 0]
 digits_pattern = re.compile(r"^[0-9]+ [0-9]+$", re.MULTILINE)
 
-gifs = ['https://t.me/randchoosecont/80',
-        'https://t.me/randchoosecont/81']
-
+numbers = [0, 0]
+gifs = ['https://t.me/randchoosecont/80', 'https://t.me/randchoosecont/81']
 lang_previous_message_id = 0
+random_num_previous_message_id = 0
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     lg.language = lg.get_language(message.from_user.language_code)
@@ -46,8 +41,18 @@ def start_message(message):
     lang_previous_message_id = message.message_id
 
 
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    main_kb_update()
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=lg.help[lg.language],
+        reply_markup=main_kb,
+        parse_mode=telegram.ParseMode.MARKDOWN_V2
+    )
 
-# keyboards:
+
+# keyboards
 main_kb = types.ReplyKeyboardMarkup(True)
 rand_num_inline_kb = telebot.types.InlineKeyboardMarkup(True)
 rand_num_chat_kb = types.InlineKeyboardMarkup(True)
@@ -61,6 +66,7 @@ language_kb.add(
 )
 
 
+# keyboards' updates
 def main_kb_update():
     global main_kb
     main_kb = types.ReplyKeyboardMarkup(True)
@@ -146,13 +152,14 @@ def empty_query(query):
     bot.answer_inline_query(query.id, [inline_help], cache_time=1)
 
 
+# inline functions
 @bot.inline_handler(func=lambda query: len(query.query) > 0)
 def content_query(query):
-    coin_flipping_query(query)
-    random_number_query(query)
+    inline_coin_flipping(query)
+    inline_random_number(query)
 
 
-def random_number_query(query):
+def inline_random_number(query):
     global numbers
     try:
         matches = re.search(digits_pattern, query.query)
@@ -178,7 +185,7 @@ def random_number_query(query):
         pass
 
 
-def coin_flipping_query(query):
+def inline_coin_flipping(query):
     lg.language = find_language(query.from_user.id)
     if query.query.lower() == 'c' or query.query.lower() == 'с':
         cf_img = "AgACAgIAAxkBAAMMXsGMo98G4qKKKji8dOKncgABIkk7AAJArjEbZxsJSoAjAwAB6dfQzUxoyw4ABAEAAwIAA20AA5nIBQABGQQ"
@@ -193,78 +200,7 @@ def coin_flipping_query(query):
         bot.answer_inline_query(query.id, [coin_flipping], cache_time=1)
 
 
-def random_number(message):
-    global numbers
-    try:
-        matches = re.search(digits_pattern, message.text)
-    except AttributeError:
-        pass
-    try:
-        numbers[0], numbers[1] = matches.group().split()
-        rand_num_chat_kb_update()
-        bot.send_message(message.chat.id,
-                         text=lg.random_num_chat_start[lg.language].format(numbers[0], numbers[1]),
-                         reply_markup=rand_num_chat_kb,
-                         parse_mode=telegram.ParseMode.MARKDOWN_V2)
-    except AttributeError:
-        if message.text != lg.coin_flipping_button[lg.language] \
-                and message.text != lg.switch_language[lg.language]:
-            bot.send_message(
-                chat_id=message.chat.id,
-                text=lg.incorrect_numbers[lg.language],
-                parse_mode=telegram.ParseMode.MARKDOWN_V2
-            )
-            global random_num_previous_message_id
-            random_num_previous_message_id = message.message_id
-        else:
-            pass
-
-
-def choose_language(message):
-    bot.send_message(message.chat.id, lg.language_choose[lg.language], reply_markup=language_kb)
-
-
-@bot.message_handler(commands=['help'])
-def start_message(message):
-    main_kb_update()
-    bot.send_message(
-        chat_id=message.chat.id,
-        text=lg.help[lg.language],
-        reply_markup=main_kb,
-        parse_mode=telegram.ParseMode.MARKDOWN_V2
-    )
-
-
-def set_language(message, name):
-    collection.insert_one({
-        "_id": message.from_user.id,
-        "user_name": name,
-        "lang_code": lg.language
-    })
-
-
-def update_language(message):
-    collection.update_one(
-        {"_id": message.from_user.id},
-        {
-            "$set": {
-                "lang_code": lg.language
-            }
-        }
-    )
-
-
-def find_language(user_id):
-    cursor = collection.find({"_id": user_id})
-    for item in cursor:
-        return item["lang_code"]
-
-
-# @bot.message_handler(content_types=['photo'])
-# def aaaa(message):
-#     bot.send_message(message.chat.id, message.photo)
-random_num_previous_message_id = 0
-
+# bot's chat functions
 @bot.message_handler(content_types=['text'])
 def menu(message):
     global random_num_previous_message_id
@@ -303,29 +239,55 @@ def menu(message):
             parse_mode=telegram.ParseMode.MARKDOWN_V2
         )
     if message.message_id - 2 == random_num_previous_message_id:
-        random_number(message)
-    # else:
-    #     bot.send_message(message.chat.id, random.choice(lg.emojis))
-    # if message.text.split()[0] == "V69N72M95S02":
-    #     cursor = collection.find()
-    #     for item in cursor:
-    #         bot.send_message(message.chat.id, 'sss')
+        chat_random_number(message)
 
 
+def chat_random_number(message):
+    global numbers
+    try:
+        matches = re.search(digits_pattern, message.text)
+    except AttributeError:
+        pass
+    try:
+        numbers[0], numbers[1] = matches.group().split()
+        rand_num_chat_kb_update()
+        bot.send_message(message.chat.id,
+                         text=lg.random_num_chat_start[lg.language].format(numbers[0], numbers[1]),
+                         reply_markup=rand_num_chat_kb,
+                         parse_mode=telegram.ParseMode.MARKDOWN_V2)
+    except AttributeError:
+        if message.text != lg.coin_flipping_button[lg.language] \
+                and message.text != lg.switch_language[lg.language]:
+            bot.send_message(
+                chat_id=message.chat.id,
+                text=lg.incorrect_numbers[lg.language],
+                parse_mode=telegram.ParseMode.MARKDOWN_V2
+            )
+            global random_num_previous_message_id
+            random_num_previous_message_id = message.message_id
+        else:
+            pass
+
+
+def choose_language(message):
+    bot.send_message(message.chat.id, lg.language_choose[lg.language], reply_markup=language_kb)
+
+
+# callback function
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     result1 = str(random.randint(int(numbers[0]), int(numbers[1])))
     result = list(result1)
-    emodji_result = []
+    emoji_result = []
     for res in result:
-        emodji_result.append("{!s}️⃣".format(res))
+        emoji_result.append("{!s}️⃣".format(res))
     if call.message:
         if call.data == "start_rand_nums":
             main_kb_update()
             bot.edit_message_text(
                 chat_id=call.message.chat.id,
                 message_id=call.message.message_id,
-                text=lg.result[lg.language].format(numbers[0], numbers[1], ''.join(emodji_result)),
+                text=lg.result[lg.language].format(numbers[0], numbers[1], ''.join(emoji_result)),
                 parse_mode=telegram.ParseMode.MARKDOWN_V2
             )
             bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text=result1)
@@ -352,7 +314,7 @@ def callback_inline(call):
         if call.data == "start_rand_nums":
             bot.edit_message_text(
                 inline_message_id=call.inline_message_id,
-                text=lg.result[lg.language].format(numbers[0], numbers[1], ''.join(emodji_result)),
+                text=lg.result[lg.language].format(numbers[0], numbers[1], ''.join(emoji_result)),
                 parse_mode=telegram.ParseMode.MARKDOWN_V2
             )
             bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text=result1)
@@ -375,9 +337,37 @@ def callback_inline(call):
             bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text=word)
 
 
+# mongo functions
+def set_language(message, name):
+    collection.insert_one({
+        "_id": message.from_user.id,
+        "user_name": name,
+        "lang_code": lg.language
+    })
+
+
+def update_language(message):
+    collection.update_one(
+        {
+            "_id": message.from_user.id
+        },
+        {
+            "$set": {
+                "lang_code": lg.language
+            }
+        }
+    )
+
+
+def find_language(user_id):
+    cursor = collection.find({"_id": user_id})
+    for item in cursor:
+        return item["lang_code"]
+
+
+# webhook functions
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
-
 
 
 @server.route('/1099895310:AAHaLPowlJMPqGwsDwG27D45lvdqo8goNkw', methods=['POST'])
